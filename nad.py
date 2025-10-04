d6061c5c4887066dcb938dbe3f610aaa62253808
LIYA TOMY <liyatomy26@gmail.com>
2025-10-04 22:01:21 +0530

    Add files via upload

---
+++ nad.py
@@ -0,0 +1,6 @@
+for i in range(1,6):
+    for j in range(1,6):
+        if i==1 or j==3 and i==2 or j==3 and i==3 or j==3 and i==4 or j==2 and i==5:
+            print("\t*",end="")
+        else:
+            print("\n")
