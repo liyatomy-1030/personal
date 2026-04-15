547cd39a3d19bc8ffbf508e95919b877606d3b46
LIYA TOMY <liyatomy26@gmail.com>
2026-04-15 21:40:23 +0530

    Add files via upload

---
+++ personel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo " JJJJJ   OOOOO   H   H   N   N "
+echo "   J    O     O  H   H   NN  N "
+echo "   J    O     O  HHHHH   N N N "
+echo " J J    O     O  H   H   N  NN "
+echo "  J      OOOOO   H   H   N   N "
